0d75bcf4d4b85af84a53cc498540c4331fcd5c0c
Afrimo <Afrimthaqi1401@yahoo.no>
2023-08-10 17:31:25 +0200

    Implementing lecture code (modifications will be implemented)

---
+++ server/moviesApi.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+export function MoviesApi(db){
+    const api = express.Router();
+
+    api.get("/", async (req, res) =>{
+
+        const collection = db.collection("movies")
+        console.log(collection.title)
+
+        const movies = await db
+            .collection("movies")
+            .find({})
+            .map(({ title, year, plot }) => ({ title, year, plot }))
+            .toArray();
+
+        res.json(movies)
+    })
+
+    api.post("/", (req, res) => {
+        const {title, year, plot} = req.body;
+
+        db.collection("movies").insertOne({ title, year, plot })
+
+        res.sendStatus(284)
+    })
+
+    return api;
+}
